78c7ea8975997957464d31b5ae1b8d06b39f40a8
Ivan Copeland <ivan.copeland2015@gmail.com>
2025-05-01 14:27:34 +0200

    Screen 2 test

--- app/src/androidTest/java/com/example/imad5112a2_2024_v2/ReportTest.kt
+++ app/src/androidTest/java/com/example/imad5112a2_2024_v2/ReportTest.kt
@@ -1,25 +1,32 @@
 package com.example.imad5112a2_2024_v2
 
+import android.content.Intent
+import android.widget.TextView
 import androidx.test.core.app.ActivityScenario
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import org.junit.Assert.*
-import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
 
 @RunWith(AndroidJUnit4::class)
 class ReportTest {
-    private lateinit var activityScenario: ActivityScenario<Report>
-    @Before
-    fun setup() {
-        // Launch the activity before each test
-        activityScenario = ActivityScenario.launch(Report::class.java)
-    }
 
     @Test
     fun test1() {
-        activityScenario.onActivity { activity ->
-            //
+        val intent = Intent(Intent.ACTION_MAIN).apply {
+            setClassName(
+                "com.example.imad5112a2_2024_v1",
+                "com.example.imad5112a2_2024_v1.Report"
+            )
+            putExtra("daysArray", arrayOf("Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"))
+            putExtra("gramsArray", intArrayOf(100, 100, 100, 100, 100, 100, 100))
+        }
+
+        val scenario = ActivityScenario.launch<Report>(intent)
+
+        scenario.onActivity { activity ->
+            val reportView = activity.findViewById<TextView>(R.id.reportTextView)
+            assertTrue(reportView.text.contains("Total: 700"))
         }
     }
 }
