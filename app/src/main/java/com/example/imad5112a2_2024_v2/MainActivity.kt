765024a89d14a0f63f878b6dac4ac0d8febe422d
Ivan Copeland <ivan.copeland2015@gmail.com>
2025-05-01 14:05:05 +0200

    First screen

--- app/src/main/java/com/example/imad5112a2_2024_v2/MainActivity.kt
+++ app/src/main/java/com/example/imad5112a2_2024_v2/MainActivity.kt
@@ -1,11 +1,57 @@
 package com.example.imad5112a2_2024_v2
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+
+private val days = arrayOf<String>(
+    "Monday", "Tuesday", "Wednesday", "Thursday",
+    "Friday", "Saturday", "Sunday"
+)
+
+private val grams = arrayOf<Int>(0, 0, 0, 0, 0, 0, 0)
+private var currentIndex = 0
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        val dayTextView = findViewById<TextView>(R.id.dayTextView)
+        val errorTextView = findViewById<TextView>(R.id.errorTextView)
+        val inputEditText = findViewById<EditText>(R.id.editTextGrams)
+        val nextButton = findViewById<Button>(R.id.nextButton)
+
+        dayTextView.text = "Enter grams of food for: ${days[0]}"
+
+        nextButton.setOnClickListener {
+
+            val value = inputEditText.text.toString().toIntOrNull()
+
+            if (value != null) {
+                errorTextView.text = "";
+                grams[currentIndex] = value
+                currentIndex += 1
+
+                if (currentIndex < 7) {
+                    dayTextView.text = "Enter grams of food for: ${days[currentIndex]}"
+                    inputEditText.text = null;
+                } else {
+                    // All days done, go to report screen
+                    val intent = Intent(this, Report::class.java)
+                    intent.putExtra("daysArray", days)
+                    intent.putExtra("gramsArray", grams.toIntArray()) // convert to primitive array
+                    startActivity(intent)
+                }
+            } else {
+                errorTextView.text = "Please enter a number"
+            }
+        }
+
+
     }
 }
