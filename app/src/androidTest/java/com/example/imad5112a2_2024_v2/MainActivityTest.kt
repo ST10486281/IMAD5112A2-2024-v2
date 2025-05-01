34cee591f0048734536a9adea487749083b9f54a
Ivan Copeland <ivan.copeland2015@gmail.com>
2025-05-01 14:13:20 +0200

    Added both tests

---
+++ app/src/androidTest/java/com/example/imad5112a2_2024_v2/MainActivityTest.kt
@@ -0,0 +1,10 @@
+package com.example.imad5112a2_2024_v2
+
+import org.junit.jupiter.api.Assertions.*
+
+class MainActivityTest {
+
+    @org.junit.jupiter.api.BeforeEach
+    fun setUp() {
+    }
+}
