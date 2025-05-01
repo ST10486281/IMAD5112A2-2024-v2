9e87b1fb9602662ae7bd9b46ea22d33c0e9f7745
Ivan Copeland <ivan.copeland2015@gmail.com>
2025-05-01 14:23:23 +0200

    Test for screen 2

--- app/src/androidTest/java/com/example/imad5112a2_2024_v2/ReportTest.kt
+++ app/src/androidTest/java/com/example/imad5112a2_2024_v2/ReportTest.kt
@@ -1,10 +1,25 @@
 package com.example.imad5112a2_2024_v2
 
-import org.junit.jupiter.api.Assertions.*
+import androidx.test.core.app.ActivityScenario
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import org.junit.Assert.*
+import org.junit.Before
+import org.junit.Test
+import org.junit.runner.RunWith
 
+@RunWith(AndroidJUnit4::class)
 class ReportTest {
+    private lateinit var activityScenario: ActivityScenario<Report>
+    @Before
+    fun setup() {
+        // Launch the activity before each test
+        activityScenario = ActivityScenario.launch(Report::class.java)
+    }
 
-    @org.junit.jupiter.api.BeforeEach
-    fun setUp() {
+    @Test
+    fun test1() {
+        activityScenario.onActivity { activity ->
+            //
+        }
     }
 }
