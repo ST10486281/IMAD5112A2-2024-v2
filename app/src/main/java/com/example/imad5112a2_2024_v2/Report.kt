3942a13f39adf6f4445abb45a2a5bac9931f2537
Ivan Copeland <ivan.copeland2015@gmail.com>
2025-05-01 13:44:17 +0200

    Created new screen

---
+++ app/src/main/java/com/example/imad5112a2_2024_v2/Report.kt
@@ -0,0 +1,11 @@
+package com.example.imad5112a2_2024_v2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Report : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_report)
+    }
+}
